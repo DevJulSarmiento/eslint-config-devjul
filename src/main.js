852be9234c0886eb501242687342a8d13ae5388d
Habid Enrique Manzur Restrepo <habidmanzur@outlook.com>
2025-08-12 16:27:49 -0500

    feat: initial commit

---
+++ src/main.js
@@ -0,0 +1,70 @@
+import jsdoc from 'eslint-plugin-jsdoc';
+import js from '@eslint/js';
+import { NODE_RULES, REACT_RULES } from './rules';
+
+/** @type {import("eslint").Linter.Config[]} */
+export const base = [
+  jsdoc.configs['flat/recommended-error'],
+  {
+    name: "javascript/recommended-rules",
+    files: ["**/*.js"],
+    plugins: {
+      js,
+      jsdoc,
+      '@stylistic/js': stylisticJs,
+    },
+    extends: ["js/recommended"],
+    rules: {
+      "max-params": ["error", 4],
+      "new-cap": ["error", { "capIsNew": false }],
+      ...NODE_RULES
+    }
+  },
+]
+
+/** @type {import("eslint").Linter.Config[]} */
+export const ignores = [
+  {
+    ignores: [
+      "*.config.*",
+      "**/*.d.ts",
+      "**/*.min.js",
+      "build/**",
+      "coverage/**",
+      "dist/**",
+      "html/**",
+    ]
+  }
+]
+
+/** @type {import("eslint").Linter.Config[]} */
+export const js = [
+  ...base,
+  ...ignores
+]
+
+/** @type {import("eslint").Linter.Config[]} */
+export const react = [
+  ...base,
+  tailwind.configs["flat/recommended"],
+  {
+    name: "react-tailwind/recommended-rules",
+    files: ['/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: REACT_RULES,
+  },
+  ...ignores
+]
---
+++ src/rules.js
@@ -0,0 +1,49 @@
+import js from '@eslint/js';
+
+export const NODE_RULES = {
+  ...js.configs.recommended.rules,
+  complexity: ['error', { max: 6, variant: 'modified' }],
+  'no-unused-vars': [
+    'error',
+    {
+      vars: 'all',
+      args: 'after-used',
+      ignoreRestSiblings: false,
+      varsIgnorePattern: '^_',
+      argsIgnorePattern: '^_',
+    },
+  ],
+  'no-useless-catch': 'error',
+
+  // stylistic
+  '@stylistic/js/indent': ['error', 2],
+
+  // JSDoc
+  'jsdoc/check-alignment': 'warn',
+  'jsdoc/check-param-names': 'error',
+  'jsdoc/check-tag-names': 'error',
+  'jsdoc/check-types': 'warn',
+  'jsdoc/require-example': [
+    'error',
+    {
+      contexts: [
+        'FunctionDeclaration',
+        'MethodDefinition',
+        'ClassDeclaration',
+        'ArrowFunctionExpression',
+        'FunctionExpression',
+      ],
+    },
+  ],
+  'jsdoc/no-defaults': 'off',
+  'jsdoc/require-param': 'error',
+  'jsdoc/require-param-description': 'error',
+  'jsdoc/require-param-type': 'error',
+  'jsdoc/require-returns': 'error',
+  'jsdoc/require-returns-description': 'error',
+  'jsdoc/require-returns-type': 'error',
+}
+
+export const REACT_RULES = {
+
+}
